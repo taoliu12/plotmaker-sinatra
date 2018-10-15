91a316b77e94d4509292e6ea61065d81f9619a90
kev112 <werewaswas@live.com>
2018-10-14 23:23:01 -0700

    add welcome and layout erb views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,22 @@ class UsersController < ApplicationController
         redirect to :plots
     end
 
+    get '/login' do 
+        erb :'users/login'
+    end
+
+    post '/login' do 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to '/plots'
+        else
+            redirect to '/'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to '/'
+    end
 end
