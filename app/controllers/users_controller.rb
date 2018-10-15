03c40a959fa3913abfcbc4889958711310dbe3de
kev112 <werewaswas@live.com>
2018-10-14 21:55:24 -0700

    add user signup route POST method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  get '/signup' do 
-      erb :'users/new'
-  end
+    get '/signup' do 
+        erb :'users/new'
+    end
 
-  
+    post '/signup' do 
+        user = User.create(params)
+        session[:user_id] = user.id
+        redirect to :plots
+    end
+
 end
