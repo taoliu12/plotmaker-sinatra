08b08e43558a4e31ae2eb1fc05196c9eeb2a2137
kev112 <werewaswas@live.com>
2018-10-17 16:32:35 -0700

    add plots index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,14 +26,14 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect to '/plots'
+            redirect '/plots'
         else
-            redirect to '/'
+            redirect '/'
         end
     end
 
     get '/logout' do
         session.clear
-        redirect to '/'
+        redirect '/'
     end
 end
