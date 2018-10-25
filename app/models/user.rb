a5750fa2145255b9d365c155287cfb63e63cde65
kev112 <werewaswas@live.com>
2018-10-25 00:00:50 -0700

    email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     has_many :plots
     has_secure_password
     validates :username, presence: :true, uniqueness: true
-    validates :email, presence: :true
+    validates :email, presence: :true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
